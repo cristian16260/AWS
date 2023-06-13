bc4db3237586f811c1d747377aa3baffb5a5c485
James Noria <jnoria.dev@gmail.com>
2023-06-12 22:14:30 -0500

    feat: new architecture

--- src/convert.js
+++ src/convert.js
@@ -1,20 +1,28 @@
-const convert = async (event,cambio) => {
-    // const {monto} = JSON.parse(event.body)
+const convertCoins = async (event) => {
+  const cambio = JSON.parse(event.body.cambio);
+  const monto = JSON.parse(event.body.monto);
 
-    const res  = await fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${cambio}/pen.json`);
-    const data = await res.json()
+  const res = await fetch(
+    `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${cambio}/pen.json`
+  );
+  const data = await res.json();
 
-    const sol = event * (data.pen)
-    const f = new Intl.NumberFormat('es-PE',{
-        style: 'currency',
-        currency: 'PEN',
-        minimumFractionDigits: 2
-    })
-    const soles = (f.format(sol))
-    console.log(soles)
-    return{
-        status: 200,
-        body: JSON.stringify(soles)
-    }
-}
-convert(10,'usd')
+  const sol = monto * data.pen;
+  const f = new Intl.NumberFormat('es-PE', {
+    style: 'currency',
+    currency: 'PEN',
+    minimumFractionDigits: 2,
+  });
+  const soles = f.format(sol);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: `El cambio de ${cambio} a soles es: ${soles}`,
+      },
+      null,
+      2
+    ),
+  };
+};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,51 @@
-module.exports.handler = async (event) => {
+module.exports.sayMyName = async (event) => {
+
+  const bodyEvent = JSON.parse(event.body); 
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Go Serverless v3.0! Your function executed successfully!",
+        message: `Hello ${bodyEvent.nombre}`,
+        body: bodyEvent,
         input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
+
+module.exports.convertCoins = async (event) => {
+
+  console.log(event.body)
+  
+  const cambio = JSON.parse(event.body).cambio;
+  const monto = JSON.parse(event.body).monto;
+
+  console.log({cambio, monto});
+  
+  const res = await fetch(
+    `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${cambio}/pen.json`
+  );
+  const data = await res.json();
+
+  console.log(data);
+
+  const sol = monto * data.pen;
+  const f = new Intl.NumberFormat('es-PE', {
+    style: 'currency',
+    currency: 'PEN',
+    minimumFractionDigits: 2,
+  });
+  const soles = f.format(sol);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: `El cambio de ${cambio} a soles es: ${soles}`,
+        event: event,
       },
       null,
       2
